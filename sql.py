67cdd061a9a2b51f51b178c35a9577d592e77596
venkatesh-kunchala <kunchalavenkatesh06@gmail.com>
2024-01-27 14:27:40 +0530

    sql ds updated

---
+++ sql.py
@@ -0,0 +1,82 @@
+from dotenv import load_dotenv
+load_dotenv() ## load all the environemnt variables
+
+import streamlit as st
+import os
+import sqlite3
+
+import google.generativeai as genai
+## Configure Genai Key
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function To Load Google Gemini Model and provide queries as response
+
+def get_gemini_response(question,prompt):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content([prompt[0],question])
+    return response.text
+
+## Fucntion To retrieve query from the database
+
+def read_sql_query(sql,db):
+    try:
+        conn=sqlite3.connect(db)
+        cur=conn.cursor()
+        cur.execute(sql)
+        rows=cur.fetchall()
+        for row in rows:
+            print(row)
+        return rows
+
+    except sqlite3.Error as e:
+        print(f"Error retrieving data: {e}")
+
+    finally:
+        # Close the cursor and the connection
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+## Define Your Prompt
+prompt=[
+    """
+    You are an expert in converting English questions to SQL query!
+    The SQL database has the name STUDENT and has the following columns - NAME, CLASS, 
+    SECTION \n\nFor example,\nExample 1 - How many entries of records are present?, 
+    the SQL command will be something like this SELECT COUNT(*) FROM STUDENT ;
+    \nExample 2 - Tell me all the students studying in Data Science class?, 
+    the SQL command will be something like this 
+    SELECT * FROM STUDENT 
+    where CLASS="Data Science"; 
+    \nExample 3 - Write a query using windows function?,
+    the SQL command will be something like this 
+    SELECT id, name, grade, 
+       ROW_NUMBER() OVER (ORDER BY grade DESC) as row_num
+    FROM STUDENT;
+    also the sql code should not have ``` in beginning or end and sql word in output
+    """
+]
+
+## Streamlit App
+
+st.set_page_config(page_title="I can Retrieve Any SQL query")
+st.header("Gemini App To Retrieve SQL Queries")
+
+question=st.text_input("Input: ",key="input")
+
+submit=st.button("Ask the question")
+
+# if submit is clicked
+if submit:
+    response=get_gemini_response(question,prompt)
+    print(f"Here is the response :{response}")
+    st.text(response)
+    
+
+## response=read_sql_query(response,"student.db")
+## st.subheader("The Response is")
+## for row in response:
+##   print(row)
+## st.header(row)
